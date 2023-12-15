171bb7cb875a7c5cb6c0187e7b1d87b53b8d01a3
HafewaBargaoui <hafewa.bargaoui@gmail.com>
2023-12-15 14:22:56 +0100

    feat: :sparkles: Add initial data to tables

--- streaming.sql
+++ streaming.sql
@@ -72,3 +72,76 @@ CREATE TABLE favorite(
     FOREIGN KEY (id_user) REFERENCES users(id_user)
 );
 
+-- Ajout de données initiales dans la table 'users'
+
+INSERT INTO users (email_user, password_user) VALUES ("hafewa.bargaoui@gmail.com", "123456789");
+INSERT INTO users (email_user, password_user) VALUES ("marwa.aloui@gmail.com", "azerty");
+INSERT INTO users (email_user, password_user) VALUES ("evans.baker@hotmail.fr", "123852");
+INSERT INTO users (email_user, password_user) VALUES ("usman.white@yahoo.fr", "ytreeza");
+
+-- Ajout de données initiales dans la table 'director'
+
+INSERT INTO director (first_name_director, last_name_director) VALUES ("James", "Cameron");
+INSERT INTO director (first_name_director, last_name_director) VALUES ("Alfred", "Hitchcock");
+INSERT INTO director (first_name_director, last_name_director) VALUES ("Jack", "Clayton");
+INSERT INTO director (first_name_director, last_name_director) VALUES ("David", "Lean");
+INSERT INTO director (first_name_director, last_name_director) VALUES ("Michael", "Cimino");
+INSERT INTO director (first_name_director, last_name_director) VALUES ("Milos", "Forman");
+INSERT INTO director (first_name_director, last_name_director) VALUES ("Ridley", "Scott");
+INSERT INTO director (first_name_director, last_name_director) VALUES ("Stanley", "Kubrick");
+INSERT INTO director (first_name_director, last_name_director) VALUES ("Bryan", "Singer");
+INSERT INTO director (first_name_director, last_name_director) VALUES ("Roman", "Polanski");
+INSERT INTO director (first_name_director, last_name_director) VALUES ("Christopher", "Nolan");
+
+-- Ajout de données initiales dans la table 'movie'
+
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Titanic",230 , 1997,1);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Vertigo", 128,1958,9);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("The Innocents",100 ,1961,8);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Lawrence of Arabia",216 ,1962,9);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("The Deer Hunter",183,1978,10);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Amadeus",160 ,1984,2);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Blade Runner",117,1982,2);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Eyes Wide Shut",159,1999,3);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("The Usual Suspects",106,2004,4);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Chinatown",117,2013,5);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Boogie Nights",159,2022,6);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Annie Hall",106,1943,1);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Princess Mononoke",130,2023,10);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("The Shawshank Redemption",155,1989,7);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("American Beauty",174,2021,11);
+INSERT INTO movie(title, duration, release_year, id_director) VALUES ("Good Will Hunting ",198,2002,1);
+
+-- Ajout de données initiales dans la table 'actor'
+
+INSERT INTO actor (first_name_actor, last_name_actor,birthdate_actor)VALUES ("James", "Stewart", '1958-08-24');
+INSERT INTO actor (first_name_actor, last_name_actor,birthdate_actor)VALUES ("Ali", "Astin", '2003-12-11');
+INSERT INTO actor (first_name_actor, last_name_actor,birthdate_actor)VALUES ("Deborah", "Kerr", '1982-07-30');
+INSERT INTO actor (first_name_actor, last_name_actor,birthdate_actor)VALUES ("Peter", "OToole", '1962-06-17');
+INSERT INTO actor (first_name_actor, last_name_actor,birthdate_actor)VALUES ("Robert", "De Niro", '1942-01-25');
+INSERT INTO actor (first_name_actor, last_name_actor,birthdate_actor)VALUES ("F. Murray", "Abraham", '1976-03-12');
+INSERT INTO actor (first_name_actor, last_name_actor,birthdate_actor)VALUES ("Harrison", "FORD", '1990-02-02');
+INSERT INTO actor (first_name_actor, last_name_actor,birthdate_actor)VALUES ("Nicole", "Kidman", '1967-09-29');
+INSERT INTO actor (first_name_actor, last_name_actor,birthdate_actor)VALUES ("Jack", "Nicholson", '1973-02-01');
+INSERT INTO actor (first_name_actor, last_name_actor,birthdate_actor)VALUES ("Jack", "Nalon", '1989-07-04');
+
+-- Ajout de données initiales dans la table 'favorite'
+
+INSERT INTO favorite (id_user, id_movie) VALUES (1, 2);
+INSERT INTO favorite (id_user, id_movie) VALUES (1, 5);
+INSERT INTO favorite (id_user, id_movie) VALUES (1, 18);
+INSERT INTO favorite (id_user, id_movie) VALUES (2, 11);
+INSERT INTO favorite (id_user, id_movie) VALUES (2, 6);
+INSERT INTO favorite (id_user, id_movie) VALUES (2, 18);
+INSERT INTO favorite (id_user, id_movie) VALUES (4, 6);
+INSERT INTO favorite (id_user, id_movie) VALUES (4, 8);
+INSERT INTO favorite (id_user, id_movie) VALUES (4,9 );
+INSERT INTO favorite (id_user, id_movie) VALUES (4,17 );
+
+-- Ajout de données initiales dans la table 'perform'
+
+INSERT INTO perform (role, is_lead_role, id_actor, id_movie) VALUES ("Alice Harford",1,10,10);
+INSERT INTO perform (role, is_lead_role, id_actor, id_movie) VALUES ("Alex",1,4,10);
+INSERT INTO perform (role, is_lead_role, id_actor, id_movie) VALUES ("Miss Giddens",0,4,18);
+INSERT INTO perform (role, is_lead_role, id_actor, id_movie) VALUES ("San",0,7,10);
+INSERT INTO perform (role, is_lead_role, id_actor, id_movie) VALUES ("Older Jamal",1,11,2);
